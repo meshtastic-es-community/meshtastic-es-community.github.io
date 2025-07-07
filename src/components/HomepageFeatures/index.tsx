dfe388787dcc8ef19d002a685b0291abc257d28b
EmilioAL-Git <emilioalfarolizan@gmail.com>
2025-07-07 18:19:20 +0200

    Update index.tsx - Iconos y textos nuevos

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -12,31 +12,29 @@ type FeatureItem = {
 const FeatureList: FeatureItem[] = [
   {
     title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    Svg: require('@site/static/img/undraw_security_0ubl.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Meshtastic es una red segura y cifrada que protege tus comunicaciones mediante cifrado de extremo a extremo.
       </>
     ),
   },
   {
     title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    Svg: require('@site/static/img/undraw_broadcast_gyxl.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Permite comunicaciones de largo alcance sin necesidad de cobertura 4G, 5G ni Wi-Fi. Ideal para zonas rurales, montaña o situaciones sin infraestructura.
       </>
     ),
   },
   {
     title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    Svg: require('@site/static/img/undraw_order-coffee_pw24.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Meshtastic es una red completamente gratis que opera en frecuencias libres, sin costos ni suscripciones.
+
       </>
     ),
   },
