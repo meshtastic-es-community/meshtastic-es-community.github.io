254817652995940172e3453f5498b76a10786a45
David Martín (Noda) <davidmartindecastro@gmail.com>
2025-07-22 08:34:33 +0200

    Set configuration for production domain meshtastic.es

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,8 +16,8 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://meshtastic-es-community.github.io',
-  //url: 'https://meshtastic.es',
+  // url: 'https://meshtastic-es-community.github.io',
+  url: 'https://meshtastic.es',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
